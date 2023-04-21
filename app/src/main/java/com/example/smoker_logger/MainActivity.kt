8ea214fc4c66cdf79f90ca97e01ef0271bf9f9fe
Edgar Enuta <edgar.enuta@zuhlke.com>
2023-04-21 13:00:34 +0100

    add colour theming for calendar

--- app/src/main/java/com/example/smoker_logger/MainActivity.kt
+++ app/src/main/java/com/example/smoker_logger/MainActivity.kt
@@ -1,40 +1,99 @@
 package com.example.smoker_logger
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.ContextThemeWrapper
+import android.widget.CalendarView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.smoker_logger.ui.theme.SmokerloggerTheme
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SmokerloggerTheme {
-                // A surface container using the 'background' color from the theme
+                Log.d(MainActivity::class.toString(), "onCreate")
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    ScaffoldDemo()
                 }
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(MainActivity::class.toString(), "onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(MainActivity::class.toString(), "onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(MainActivity::class.toString(), "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(MainActivity::class.toString(), "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(MainActivity::class.toString(), "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(MainActivity::class.toString(), "onDestroy")
+    }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Compose $name!")
+fun ScaffoldDemo() {
+    Scaffold(
+        topBar = { TopAppBar(title = {Text("Smoker Logger")}, backgroundColor = MaterialTheme.colors.primarySurface)  },
+        content = { Calendar(Modifier.fillMaxSize()) },
+        bottomBar = { BottomAppBar(backgroundColor = MaterialTheme.colors.primarySurface) {} }
+    )
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun DefaultPreview() {
+fun Calendar(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val calendar = Calendar.getInstance()
+
+//    var selectedDateText by remember { mutableStateOf("") }
+//    val year = calendar[Calendar.YEAR]
+//    val month = calendar[Calendar.MONTH]
+//    val day = calendar[Calendar.DAY_OF_MONTH]
+    AndroidView(
+        { CalendarView(ContextThemeWrapper(context, R.style.Widget_CalendarView_Custom)) },
+        modifier,
+        update = { views ->
+            views.date = calendar.timeInMillis
+        }
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MainPreview() {
     SmokerloggerTheme {
-        Greeting("Android")
+        ScaffoldDemo()
     }
 }
--- app/src/main/java/com/example/smoker_logger/ui/theme/Color.kt
+++ app/src/main/java/com/example/smoker_logger/ui/theme/Color.kt
@@ -1,8 +1,9 @@
 package com.example.smoker_logger.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import com.example.smoker_logger.R
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Purple200 = Color(R.color.purple_200)
+val Purple500 = Color(R.color.purple_500)
+val Purple700 = Color(R.color.purple_700)
+val Teal200 = Color(R.color.teal_200)
